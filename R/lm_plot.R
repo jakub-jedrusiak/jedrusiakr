47af2e6e1172b55f3d62e9f2329497c38ff6e2e4
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-06-06 13:14:45 +0200

    change lm_plot

--- R/lm_plot.R
+++ R/lm_plot.R
@@ -3,6 +3,7 @@
 #' @param df a data frame
 #' @param x variable for x axis
 #' @param y variable for y axis
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> additional arguments to pass to `aes()`
 #' @param xlab label for x axis
 #' @param ylab label for y axis
 #'
@@ -10,11 +11,11 @@
 #' @export
 #'
 #' @examples
-lm_plot <- function(df, x, y, xlab, ylab) {
+lm_plot <- function(df, x, y, ..., xlab = NULL, ylab = NULL, line_colour = "#1E88E5", point_colour = "#9ECAE1") {
   df %>%
-    ggplot(aes({{ x }}, {{ y }})) +
-    geom_jitter(alpha = 0.7, colour = "#9ECAE1") +
-    geom_smooth(method = "lm", se = FALSE, colour = "#1E88E5") +
+    ggplot(aes({{ x }}, {{ y }}, ...)) +
+    geom_jitter(alpha = 0.7, colour = point_colour) +
+    geom_smooth(method = "lm", se = FALSE, colour = line_colour) +
     labs(x = xlab, y = ylab) +
     theme_minimal()
 }

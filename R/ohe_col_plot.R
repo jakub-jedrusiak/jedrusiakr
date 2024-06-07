532897ab57017d9f54a9babca9b938e7f9f363b6
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-06-07 16:56:31 +0200

    one_col_plot

---
+++ R/ohe_col_plot.R
@@ -0,0 +1,52 @@
+#' One Hot Encoding Column Plot
+#'
+#' @param df dataframe
+#' @param col column with multiple choice values
+#' @param cols_to_delete names of columns to be deleted, that is additional columns after usually a comma that isn't different to the main column
+#' @param ... "new" = "old" column rename pairs
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ohe_col_plot <- function(df, col, cols_to_delete = NULL, ..., xlab = NULL, ylab = NULL) {
+  require(rlang)
+  col <- enquo(col)
+  col_name <- expr_text(col) %>%
+    str_remove("~")
+  cols_to_delete <- enquo(cols_to_delete)
+  df <- df %>%
+    select(!!col) %>%
+    filter(!is.na(!!col))
+  nrow <- nrow(df)
+  df %>%
+    lares::ohe_commas(!!col, sep = ", *", noval = NA, remove = TRUE) %>%
+    set_names(., str_remove(names(.), paste0(col_name, "_"))) %>%
+    select(-!!cols_to_delete) %>%
+    rename(!!!list2(...)) %>%
+    pivot_longer(cols = everything(), names_to = "name", values_to = "value") %>%
+    filter(value) %>%
+    freq_table(name) %>%
+    arrange(n) %>%
+    mutate(
+      name = str_wrap(name, 20),
+      name = fct_inorder(name),
+      prop = (n / nrow) * 100
+    ) %>%
+    ggplot(aes(name, prop)) +
+    geom_col(fill = "#74C476", colour = "#006D2C") +
+    geom_label(aes(label = paste0(format(round(prop, 1), decimal.mark = ","), "%"))) +
+    scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+    labs(
+      x = xlab,
+      y = ylab
+    ) +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+      panel.grid.minor = element_blank(),
+      panel.grid.major.x = element_blank()
+    )
+}
